6a0990945d65f469898156510578c485e268d334
austinthoet <32113222+austinthoet@users.noreply.github.com>
2022-02-25 18:48:23 -0700

    Update ignite2022.js

--- ignite2022.js
+++ ignite2022.js
@@ -44,8 +44,13 @@ function getProfiles(){
 Automates the writing and sending of messages
 */
 function fireLine(){
-  document.execCommand('insertText', false, "Damn girl are you COVID positive cause you chokin me up.");
+   /** 
+    var dictionary = ['If COVID19 doesnt take you out soon, can I?', 'Can not spell quarantine without u r a q t.', 'Wanna play corona and get spread on every hard surface?', 'Is that hand sanitizer in your pocket or are you just happy to be within 6 feet of me?', 'Since all the public libraries are closed, I am checking you out instead.', 'Baby, do you need toilet paper? Because I can be your Prince Charmin', 'Yo girl are you a global pandemic because you got me feeling sick...', 'Damn girl are you COVID positive cause you chokin me up.', 'How you doin?', 'If COVID19 doesnt take you out soon, can I?']; //create dictionary of pickup lines
+    var randomNumber = Math.floor(Math.random() * dictionary.length); 
+    var message = dictionary[randomNumber]; //writes a randomly chosen pickup line
+    **/
 
+  document.execCommand('insertText', false, "Damn girl are you COVID positive cause you chokin me up."); //change message here
   var buttons = document.getElementsByTagName("button");
   buttons[11].setAttribute('id', "chatTextAreaSubmit");
   document.getElementById("chatTextAreaSubmit").disabled = false;
